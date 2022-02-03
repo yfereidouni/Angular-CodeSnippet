1cd2361a7a4d0be78ed3c75796e0a8481b15df28
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-02-03 18:24:18 +0330

    layout changed.

--- 05-recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ 05-recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Chicken Masala',
+      'Chicken parmezan and grilled cheese.',
+      'https://assets.bonappetit.com/photos/5b69f163d3d14670539a2174/1:1/w_2240,c_limit/ba-tikka-masala-2.jpg'
+    ),
+    new Recipe(
+      'Roast beef Soup',
+      'This is new taste of grilled-chicken.',
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/easy-cheap-dinners-weeknight-1604466210.jpg?crop=0.502xw:1.00xh;0.498xw,0&resize=640:*'
+    ),
+    new Recipe(
+      'Pasta alla Norma',
+      'This traditional Sicilian pasta dish with tomato sauce.',
+      'https://static.onecms.io/wp-content/uploads/sites/9/2018/09/pasta-alla-norma-XL-RECIPE1018.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
