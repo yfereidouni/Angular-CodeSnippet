92a18a7936af25f6afb538fd4c7057814711fe45
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-02-11 13:28:15 +0330

    07-code-challenge

---
+++ 07-code-challenge/src/app/game-control/game-control.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css'],
+})
+export class GameControlComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  @Output() intervalFired = new EventEmitter<number>();
+  interval: any;
+  lastNumber = 0;
+
+  onStartGame() {
+    this.interval = setInterval(() => {
+      this.intervalFired.emit(this.lastNumber + 1);
+      this.lastNumber++;
+    }, 1000);
+  }
+
+  onPauseGame() {
+    clearInterval(this.interval);
+  }
+}
