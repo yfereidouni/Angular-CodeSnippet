300e87194a6af7f357be0f848d54c81420e34707
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-02-11 14:46:19 +0330

    game-control was implemented

--- 07-code-challenge/src/app/odd/odd.component.ts
+++ 07-code-challenge/src/app/odd/odd.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-odd',
   templateUrl: './odd.component.html',
-  styleUrls: ['./odd.component.css']
+  styleUrls: ['./odd.component.css'],
 })
 export class OddComponent implements OnInit {
+  @Input() number!: number;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
