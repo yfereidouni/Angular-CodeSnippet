d3382a156009273cdcac16277e0847b1a1cacbcc
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-02-17 11:27:31 +0330

    add ingredients

--- 05-recipe-book/src/app/recipes/recipe.service.ts
+++ 05-recipe-book/src/app/recipes/recipe.service.ts
@@ -1,8 +1,10 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
-
+@Injectable()
 export class RecipeService{
     recipeSelected = new EventEmitter<Recipe>();
 
@@ -10,21 +12,40 @@ export class RecipeService{
         new Recipe(
             'Chicken Masala',
             'Chicken parmezan and grilled cheese.',
-            'https://assets.bonappetit.com/photos/5b69f163d3d14670539a2174/1:1/w_2240,c_limit/ba-tikka-masala-2.jpg'
+            'https://assets.bonappetit.com/photos/5b69f163d3d14670539a2174/1:1/w_2240,c_limit/ba-tikka-masala-2.jpg',
+            [
+                new Ingredient('Meat',2),
+                new Ingredient('French Fries',20)
+            ]
         ),
         new Recipe(
             'Roast beef Soup',
             'This is new taste of grilled-chicken.',
-            'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg'
+            'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg',
+            [
+                new Ingredient('Buns',2),
+                new Ingredient('Meat',1)
+            ]
         ),
         new Recipe(
             'Pasta alla Norma',
             'This traditional Sicilian pasta dish with tomato sauce.',
-            'https://static.onecms.io/wp-content/uploads/sites/9/2018/09/pasta-alla-norma-XL-RECIPE1018.jpg'
+            'https://static.onecms.io/wp-content/uploads/sites/9/2018/09/pasta-alla-norma-XL-RECIPE1018.jpg',
+            [
+                new Ingredient('Beans',2),
+                new Ingredient('Spagetti',1),
+                new Ingredient('Fish',1)
+            ]
         ),
     ];
 
+    constructor(private slService:ShoppingListService){}
+
     getRecipes(){
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]){
+        this.slService.addIngredients(ingredients);
     }
 }
--- 05-recipe-book/src/app/shopping-list/shopping-list.service.ts
+++ 05-recipe-book/src/app/shopping-list/shopping-list.service.ts
@@ -1,20 +1,27 @@
 import { EventEmitter } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 
-export class ShoppingListService{
-ingredientsChanged = new EventEmitter<Ingredient[]>();
-
+export class ShoppingListService {
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
     private ingredients: Ingredient[] = [
-        new Ingredient('Apples', 5),
-        new Ingredient('Tomatos', 10),
+      new Ingredient('Apples', 5),
+      new Ingredient('Tomatoes', 10),
     ];
     
-    getIngredients(){
-        return this.ingredients.slice();
+    getIngredients() {
+      return this.ingredients.slice();
+    }
+    
+    addIngredient(ingredient: Ingredient) {
+      this.ingredients.push(ingredient);
+      this.ingredientsChanged.emit(this.ingredients.slice());
     }
 
-    addIngredient(ingredient:Ingredient){
-        this.ingredients.push(ingredient);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+    addIngredients(ingredients: Ingredient[]) {
+      // for (let ingredient of ingredients) {
+      //   this.addIngredient(ingredient);
+      // }
+      this.ingredients.push(...ingredients);
+      this.ingredientsChanged.emit(this.ingredients.slice());
     }
 }
