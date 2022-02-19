64a98f8bc5163cb818b3706c5aea96824651a74d
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-02-19 21:16:20 +0330

    redirect by wildCard

--- 11-routes-pages/src/app/app.module.ts
+++ 11-routes-pages/src/app/app.module.ts
@@ -12,14 +12,19 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes : Routes = [
   {path: ''      , component: HomeComponent},
-  {path:'users'  , component: UsersComponent},
-  {path:'users/:id/:name'  , component: UserComponent},
-  {path:'servers', component: ServersComponent},
-  {path:'servers/:id', component: ServerComponent},
-  {path:'servers/:id/edit', component: EditServerComponent}
+  {path:'users'  , component: UsersComponent, children:[
+    {path:':id/:name'  , component: UserComponent},
+  ]},
+  {path:'servers', component: ServersComponent,children:[
+    {path:':id', component: ServerComponent},
+    {path:':id/edit', component: EditServerComponent},
+  ]},
+  {path: 'not-found',component:PageNotFoundComponent},
+  {path: '**',redirectTo:'not-found'},
 ];
 
 @NgModule({
@@ -31,6 +36,7 @@ const appRoutes : Routes = [
     ServersComponent,
     ServerComponent,
     EditServerComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
--- 11-routes-pages/src/app/servers/edit-server/edit-server.component.ts
+++ 11-routes-pages/src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -19,6 +19,7 @@ export class EditServerComponent implements OnInit {
 
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(private serversService: ServersService,
               private route:ActivatedRoute) { }
@@ -26,7 +27,11 @@ export class EditServerComponent implements OnInit {
   ngOnInit() {
     console.log(this.route.snapshot.queryParams);
     console.log(this.route.snapshot.fragment);
-    this.route.queryParams.subscribe();
+    this.route.queryParams.subscribe(
+      (queryParams:Params) => {
+        this.allowEdit = queryParams['allowEdit'] === '1'? true:false;
+      }
+    );
     this.route.fragment.subscribe();
     this.server = this.serversService.getServer(1)!;
     this.serverName = this.server.name;
